18d02afa9b75e9c8209d1f2e39d7d5559eef2afe
pedro-batista <peddrobatista2022@gmail.com>
2024-04-04 15:22:18 -0300

    Excluindo dados...

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,16 +10,13 @@ public class Programa {
 	
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
 		Pessoa p = em.find(Pessoa.class, 2);
-		
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 		System.out.println("Pronto!");
 		em.close();
 		emf.close();
